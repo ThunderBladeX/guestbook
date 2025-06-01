b276606ae0a09f0dda25e08d5557a0eb64778fd7
ThunderBladeX <magicalpurplehearth@gmail.com>
2025-06-01 14:23:30 +0700

    Update guestbook.py

--- guestbook.py
+++ guestbook.py
@@ -259,3 +259,7 @@ def internal_error(error):
         'success': False,
         'error': 'Internal server error'
     }), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
